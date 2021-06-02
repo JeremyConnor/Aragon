40ed339b8ab91b7cfe91cb88eef33ac0233fdbb0
JeremyConnor <ultra31magnus@gmail.com>
2021-06-02 16:23:58 +0530

    Making the contract ready for the app

--- contracts/CounterApp.sol
+++ contracts/CounterApp.sol
@@ -1,8 +1,12 @@
 import "@aragon/os/contracts/lib/math/SafeMath.sol";
+import "@aragon/os/contracts/apps/AragonApp.sol";
 
-
-contract CounterApp {
+contract CounterApp is AragonApp {
     using SafeMath for uint256;
+
+    /// Definiing roles to be made accessible by the app
+    bytes32 constant public INCREMENT_ROLE = keccak256("INCREMENT_ROLE");
+    bytes32 constant public DECREMENT_ROLE = keccak256("DECREMENT_ROLE");
 
     /// Events
     event Increment(address indexed entity, uint256 step);
